dc270b2e3c00619c83ae14f398e75d93d9358345
Shujaat Azim <shujaat.azim@gmail.com>
2025-09-01 00:00:00 -0400

    check for non-int entries

--- multipy.py
+++ multipy.py
@@ -2,14 +2,15 @@ print("===      MultiPy         ===")
 print("===   By Shujaat Azim    ===")
 print("\n")
 
-requested_int = int(input("Enter an integer between 1 and 10: "))
+requested_int = input("Enter an integer between 1 and 10: ")
+
+while not requested_int.isdigit() or int(requested_int) > 10 or int(requested_int) < 1:
+    requested_int = input("Please enter an integer between 1 and 10: ")
+    
 print("\n")
 
-while requested_int > 10 or requested_int < 1:
-    requested_int = int(input("Please enter an integer between 1 and 10: "))
-    
-for x in range(1, requested_int + 1):
+for x in range(1, int(requested_int) + 1):
   row = ""
-  for y in range(1, requested_int + 1):
+  for y in range(1, int(requested_int) + 1):
     row = row + str(x * y) + "\t"
   print(row)
